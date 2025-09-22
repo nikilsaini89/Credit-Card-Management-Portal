8996ebe64c730a4cd38415c6a43abcc1d1f7b70b
Nikil Saini <nikilsaini74@gmail.com>
2025-09-23 00:48:13 +0530

    feat: implement CreditCard, CardType and User entity

---
+++ backend/src/main/java/com/ccms/portal/model/UserEntity.java
@@ -0,0 +1,38 @@
+package com.ccms.portal.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false, length = 15)
+    private String phoneNumber;
+    private String passwordHash;
+    private String address;
+    private Double annualIncome;
+    private boolean isEligibleBnpl;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CreditCardEntity> creditCards = new ArrayList<>();
+
+
+}
