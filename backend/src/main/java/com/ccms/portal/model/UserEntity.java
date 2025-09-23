27343f7773a49a8940c3eb833cfebe59fe126eab
Nikil Saini <nikilsaini74@gmail.com>
2025-09-23 10:30:24 +0530

    refactor: rename user entity name to users

--- backend/src/main/java/com/ccms/portal/model/UserEntity.java
+++ backend/src/main/java/com/ccms/portal/model/UserEntity.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
