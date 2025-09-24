20b82d81aceee4bfdea91c4e157fa4784a762d9b
Anchal.j <anchal.j@ithelpdesks-MacBook-Pro.local>
2025-09-24 10:45:56 +0530

    feat: implement JWT Auth

--- backend/src/main/java/com/ccms/portal/repository/CardTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ccms.portal.repository;
-
-import com.ccms.portal.model.CardTypeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CardTypeRepository extends JpaRepository<CardTypeEntity, Long> {
-}
