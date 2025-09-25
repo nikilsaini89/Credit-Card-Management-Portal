59b8165fbc22f10c66731716b5de2ff7e268b3b3
Anchal.j <anchal.j@ithelpdesks-MacBook-Pro.local>
2025-09-25 11:09:06 +0530

    added the login page

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory,type RouteRecordRaw } from 'vue-router'
+import store from '../store/index'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue'),
+    meta: { requiresGuest: true }
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import('../views/Users/Dashboard.vue'),
+    meta: { requiresAuth: true }
+  }
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = store.getters['auth/isAuthenticated']
+  
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login')
+  } else if (to.meta.requiresGuest && isAuthenticated) {
+    next('/dashboard')
+  } else {
+    next()
+  }
+})
+
+export default router
