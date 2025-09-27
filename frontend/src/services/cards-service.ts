8351077be6e36bb9e06f9198ac3873ccf88fceb7
Nikil Saini <nikilsaini74@gmail.com>
2025-09-28 00:15:26 +0530

    fix: change add token location in card service

--- frontend/src/services/cards-service.ts
+++ frontend/src/services/cards-service.ts
@@ -5,9 +5,9 @@ import { getUserIdFromToken } from '../utils/getTokenData';
 
 const BASE_URL = 'http://localhost:8080';
 
-const token = localStorage.getItem("token");
 
 export const getCards = async (): Promise<CreditCard[]> => {
+  const token = localStorage.getItem("token");
   const userId = getUserIdFromToken();
   try {
     const res = await axios.get<CreditCard[]>(`${BASE_URL}/cards`, {
@@ -22,6 +22,7 @@ export const getCards = async (): Promise<CreditCard[]> => {
 };
 
 export const getCardTypes = async (): Promise<CardType[]> => {
+  const token = localStorage.getItem("token");
   const userId = getUserIdFromToken();
   try {
     const res = await axios.get<CardType[]>(`${BASE_URL}/cards/type`, {
@@ -40,6 +41,7 @@ export const updateCardStatus = async (
   newStatus: string
 ): Promise<CreditCard | null> => {
   try {
+    const token = localStorage.getItem("token");
     const res = await axios.put<CreditCard>(
       `${BASE_URL}/cards/${cardId}/status`,
       {
@@ -59,6 +61,8 @@ export const updateCardStatus = async (
 
 export const getCardApplications = async (): Promise<CardApplication[]> => {
   try {
+    const token = localStorage.getItem("token");
+    
     const res = await axios.get<CardApplication[]>(`${BASE_URL}/card/application`, {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -75,7 +79,8 @@ export const updateApplicationStatus = async (
   applicationId: number,
   status: 'ACCEPTED' | 'REJECTED'
 ): Promise<CardApplication | null> => {
-  try {
+  try { 
+    const token = localStorage.getItem("token");
     const res = await axios.patch<CardApplication>(
       `${BASE_URL}/card/application/${applicationId}`,
       { status },
