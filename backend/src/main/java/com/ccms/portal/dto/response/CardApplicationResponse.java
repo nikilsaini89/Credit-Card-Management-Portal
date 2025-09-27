8b075db1a19e52bcff3b902979983a1ad601bc99
sumitnegi <sumitnegi9667@gmail.com>
2025-09-27 22:34:16 +0530

    fix : frontend response for getApplication

--- backend/src/main/java/com/ccms/portal/dto/response/CardApplicationResponse.java
+++ backend/src/main/java/com/ccms/portal/dto/response/CardApplicationResponse.java
@@ -10,23 +10,28 @@ import java.time.LocalDateTime;
 @Data
 public class CardApplicationResponse {
 
-    private final Long id;
-    private final Long userId;
-    private final Long cardTypeId;
-    private final Long requestedLimit;
-    private final LocalDateTime applicationDate;
-    private final CardApplicationStatus status;
-    private Long reviewerId;
+    private Long id;
+    private Long userId;
+    private String cardTypeName;
+    private String cardNetworkType;
+    private Long requestedLimit;
+    private LocalDateTime applicationDate;
+    private String status;
+
+    private String reviewerName;
     private LocalDateTime reviewDate;
 
     public CardApplicationResponse(CardApplicationEntity entity) {
         this.id = entity.getId();
         this.userId = entity.getUserId();
-        this.cardTypeId = entity.getCardTypeId();
+        this.cardTypeName = entity.getCardType() != null ? entity.getCardType().getName() : null;
+        this.cardNetworkType = entity.getCardType() != null ? entity.getCardType().getNetworkType().name() : null;
         this.requestedLimit = entity.getRequestedLimit();
         this.applicationDate = entity.getApplicationDate();
-        this.status = entity.getStatus();
+        this.status = entity.getStatus().name();
+        this.reviewerName = entity.getReviewer() != null ? entity.getReviewer().getName() : null;  
         this.reviewDate = entity.getReviewDate();
-        this.reviewerId = entity.getReviewerId();
     }
 }
+
+
